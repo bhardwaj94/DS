8d45f49b5a4d3f22ce2d45628a1fa056704f6b0c
bhardwaj94 <bhardwaj.info2cs@gmail.com>
2019-06-28 09:10:11 +0530

    D linked list added

---
+++ DlinkedList/DoublyLL.js
@@ -0,0 +1,20 @@
+class Node{
+	constructor(element,next=null,prev=null){
+	this.element = element;
+	this.next = next;
+	this.prev = prev;
+	}
+}
+
+class LinkedList{
+	constructor(){
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
+
+append(element){
+	let node = new Node()
+
+	}
+}
